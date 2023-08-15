c15dc6cd7c94706fc25ad835bbea5740fe3417bc
deveripon <developershahadathussainripon@gmail.com>
2023-08-15 16:04:04 +0600

    Designed Woocommerce shop page

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,25 +1,54 @@
-const woocommerceItem = document.querySelectorAll(".woocommerce-item");
-const productView = document.querySelector(".product-view");
-woocommerceItem.map((item) => {
-  item.addEventListener("mouseenter", () => {
+const woocommerceItem = document.querySelectorAll(".woocommerce-item")
 
-    productView.style.top = "25%";
-
-
+// shoe view option
+function showViewOption() {
+  woocommerceItem.forEach((item) => {
+    item.addEventListener("mouseenter", function (e) {
+      let viewBox = e.target.querySelector(".product-view");
+      let viewIcon = viewBox.querySelector(".view-icon")
+      viewBox.classList.add("active")
+      viewIcon.classList.add("active")
+    })
   });
-});
-// woocommerceItem.map((item) => {
-//   item.addEventListener("mouseleave", () => {
-//     productView.map((viewButton) => {
-//       viewButton.style.top = "25%";
-//     });
 
-//   });
-// });
+}
 
-woocommerceItem.addEventListener("mouseenter", () => {
-  productView.style.top = "25%";
-});
-woocommerceItem.addEventListener("mouseleave", () => {
-  productView.style.top = "-25%";
-});
+// close view button
+function closeViewOption() {
+  woocommerceItem.forEach((item) => {
+    item.addEventListener("mouseleave", function (e) {
+      let viewBox = e.target.querySelector(".product-view");
+      let viewIcon = viewBox.querySelector(".view-icon")
+      viewBox.classList.remove("active")
+      viewIcon.classList.remove("active")
+    })
+  })
+}
+showViewOption();
+closeViewOption();
+
+// single product modal show and close
+function singleProductModalShowClose() {
+  document.addEventListener("DOMContentLoaded", function () {
+    const openModalBtn = document.querySelectorAll(".openModalBtn");
+    const modal = document.getElementById("modal");
+    const closeModalBtn = document.getElementById("closeModalBtn");
+
+    openModalBtn.forEach((item) => {
+      item.addEventListener("click", function () {
+        modal.style.display = "block";
+      });
+    });
+
+    closeModalBtn.addEventListener("click", function () {
+      modal.style.display = "none";
+    });
+
+    window.addEventListener("click", function (event) {
+      if (event.target === modal) {
+        modal.style.display = "none";
+      }
+    });
+  });
+}
+singleProductModalShowClose();
